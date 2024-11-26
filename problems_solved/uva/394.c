0bd6d57b1f976efe307f707348537df780a33de4
Thayllon Bragança <thayllonbraganca@gmail.com>
2024-11-26 08:02:47 -0300

    finishing the algorithm

--- problems_solved/uva/394.c
+++ problems_solved/uva/394.c
@@ -1,85 +1,74 @@
-/*
- *
- *
- * INPUT:
- * 3 4
- * ONE 1500 2 2 0 3 1 5
- * TWO 2000 4 3 1 4 0 5 5 10
- * THREE 3000 1 1 1 9
- * ONE 2 4
- * THREE 7
- * TWO 2 0 6
- * TWO 3 3 9
- *
- *
- * ONE          1500            2                   2                       0 3               1 5
- * Nome         base matriz     tamanho em bytes    Numero de elementos     varia de 0 a 3    varia de 1 a 5
- * */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-typedef struct{
+typedef struct {
     char an[10];
-    int bm;
-    int tb;
-    int n;
-    int t[10];
-    int d[10];
-    int c[12];
-}arr;
+    int bm;          // Base matriz
+    int tb;          // Tamanho em bytes
+    int c[11];       // Constantes para cálculo do deslocamento
+    int t[10];       // Tamanho máximo de cada dimensão
+    int n;           // Número de dimensões
+    int d[10];       // Limite inferior de cada dimensão
+} arr;
 
-int main(){
-    int n=0, nr=0;
+int main() {
+    int n = 0, nr = 0;
 
     scanf("%d %d", &n, &nr);
-    arr* ar = malloc(n*sizeof(arr));
+    arr* ar = malloc(n * sizeof(arr));
 
-    for(int i =0; i<n; i++){
-        scanf("%s%d%d%d", ar[i].an, &ar[i].bm,&ar[i].tb,&ar[i].n);
-        for(int j=0; j<ar[i].n; j++){
-            scanf("%d", &ar[i].d[j]);
-            scanf("%d", &ar[i].t[j]);
+    for (int i = 0; i < n; i++) {
+        scanf("%s %d %d %d", ar[i].an, &ar[i].bm, &ar[i].tb, &ar[i].n);
 
+        for (int j = 0; j < ar[i].n; j++) {
+            scanf("%d %d", &ar[i].d[j], &ar[i].t[j]);
         }
 
         ar[i].c[ar[i].n] = ar[i].tb;
-        ar[i].c[0] = ar[i].bm;
-        for(int j=ar[i].n-1; j>=1; j--){
-
-            ar[i].c[j] = ar[i].c[j+1]*(ar[i].t[j] - ar[i].d[j] +1);
+        for (int j = ar[i].n - 1; j >= 0; j--) {
+            ar[i].c[j] = ar[i].c[j + 1] * (ar[i].t[j] - ar[i].d[j] + 1);
         }
     }
 
     char nm[10];
-    int an;
-    int a=0;
-
-
     unsigned long tt;
-    for(int i =0; i<nr; i++){
-        scanf("%s", nm);
-        for(int j = 0; j < n; j++){
 
-            if(!strcmp(ar[j].an, nm)){
+    for (int i = 0; i < nr; i++) {
+        scanf("%s", nm);
+        int a = -1;
+
+        for (int j = 0; j < n; j++) {
+            if (!strcmp(ar[j].an, nm)) {
                 a = j;
-                tt = ar[j].c[0];
                 break;
             }
         }
-        printf("%s[", ar[a].an);
-        scanf("%d",&an);
-        printf("%d",an);
-        tt += ar[a].c[ar[a].n]*(an-ar[a].d[0]);
-        for(int j=1;j<ar[a].n; j++){
-            scanf("%d",&an);
-            tt += ar[a].c[j]*(an-ar[a].d[j]);
-            printf(", %d",an);
+
+        if (a == -1) {
+            printf("Array %s não encontrado.\n", nm);
+            continue;
         }
-        printf("] = %lu\n",tt);
+
+        tt = ar[a].bm;
+        printf("%s[", ar[a].an);
+
+        for (int j = 0; j < ar[a].n; j++) {
+            int an;
+            scanf("%d", &an);
+
+            if (an < ar[a].d[j] || an > ar[a].t[j]) {
+                printf("Índice %d fora do intervalo [%d, %d]\n", an, ar[a].d[j], ar[a].t[j]);
+                return 1;
+            }
+
+            tt += ar[a].c[j + 1] * (an - ar[a].d[j]);
+            printf("%d%s", an, (j == ar[a].n - 1) ? "" : ", ");
+        }
+
+        printf("] = %lu\n", tt);
     }
+
+    free(ar);
     return 0;
 }
---
+++ problems_solved/uva/test.cpp
@@ -0,0 +1,90 @@
+/*
+ *
+ *
+ * INPUT:
+ * 3 4
+ * ONE 1500 2 2 0 3 1 5
+ * TWO 2000 4 3 1 4 0 5 5 10
+ * THREE 3000 1 1 1 9
+ * ONE 2 4
+ * THREE 7
+ * TWO 2 0 6
+ * TWO 3 3 9
+ *
+ *
+ * ONE          1500            2                   2                       0 3               1 5
+ * Nome         base matriz     tamanho em bytes    Numero de elementos     varia de 0 a 3    varia de 1 a 5
+ * */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct{
+    char an[10];
+    int bm;
+    int tb;
+    int n;
+    int t[10];
+    int d[10];
+    int c[11];
+}arr;
+
+int main(){
+    int n=0, nr=0;
+
+    scanf("%d %d", &n, &nr);
+    arr* ar = malloc(n*sizeof(arr));
+
+    for(int i =0; i<n; i++){
+        scanf("%s%d%d%d", ar[i].an, &ar[i].bm,&ar[i].tb,&ar[i].n);
+        for(int j=0; j<ar[i].n; j++){
+            scanf("%d", &ar[i].d[j]);
+            scanf("%d", &ar[i].t[j]);
+            printf("(%d %d)", ar[i].t[j], ar[i].d[j]);
+
+        }
+
+        ar[i].c[ar[i].n] = ar[i].tb;
+        ar[i].c[0] = ar[i].bm;
+        for(int j=ar[i].n-1; j>=1; j--){
+            ar[i].c[j] = ar[i].c[j+1]*(ar[i].t[ar[i].n-j] - ar[i].d[ar[i].n-j] +1);
+            printf("%d %d %d\n", ar[i].n-j, ar[i].t[ar[i].n-j], ar[i].d[ar[i].n-j]);
+        }
+    }
+
+    char nm[10];
+    int an;
+    int a=0;
+
+
+    unsigned long tt;
+    for(int i =0; i<nr; i++){
+
+        scanf("%s", nm);
+        for(int j = 0; j < n; j++){
+
+            if(!strcmp(ar[j].an, nm)){
+                a = j;
+                tt = ar[j].c[0];
+                break;
+            }
+        }
+
+        printf("%s[", ar[a].an);
+        scanf("%d",&an);
+        printf("%d",an);
+        tt += ar[a].c[ar[a].n]*(an-ar[a].d[0]);
+
+        for(int j=1;j<ar[a].n; j++){
+            scanf("%d",&an);
+            tt += ar[a].c[j]*(an-ar[a].d[j]);
+            printf(", %d",an);
+        }
+
+        printf("] = %lu\n",tt);
+    }
+    return 0;
+}
